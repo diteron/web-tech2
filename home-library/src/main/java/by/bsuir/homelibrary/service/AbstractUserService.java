a9bc822deadbec12bd52e1e15400f842e282c183
Dzmitry Haron <pitbull9846@gmail.com>
2024-11-05 22:47:34 +0300

    Refactor password hashing

--- home-library/src/main/java/by/bsuir/homelibrary/service/AbstractUserService.java
+++ home-library/src/main/java/by/bsuir/homelibrary/service/AbstractUserService.java
@@ -1,7 +1,9 @@
 package by.bsuir.homelibrary.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.regex.Pattern;
 
@@ -14,8 +16,9 @@ import javax.crypto.spec.PBEKeySpec;
  * serves as an abstract base for specific implementations of user services.
  */
 public abstract class AbstractUserService {
-    private static final int HASH_ITERATIONS = 128;
+    private static final int HASH_ITERATIONS = 8192;    // OWASP recommends to use 600,000 iterations for PBKDF2-HMAC-SHA256
     private static final int HASH_KEY_LENGTH = 128;
+    private static final int SALT_SIZE = 16;
 
     /**
      * Checks if a user login is valid by verifying the provided login and password.
@@ -25,13 +28,7 @@ public abstract class AbstractUserService {
      * @return {@code true} if the login is valid, otherwise {@code false}
      */
     public abstract boolean isLogIn(String login, String password);
-
-    /**
-     * Validates the format of an email address.
-     *
-     * @param email the email address to validate
-     * @return {@code true} if the email format is valid, otherwise {@code false}
-     */    
+   
     protected boolean isEmailValid(String email) {
         String emaiRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
         return Pattern.compile(emaiRegex)
@@ -47,18 +44,23 @@ public abstract class AbstractUserService {
      * @throws RuntimeException if the hashing algorithm is not available or the key specification is invalid
      */    
     protected String getPasswordHash(String password) {
-        byte[] salt = new byte[16];
-        salt = "ggg".getBytes();        // TODO: Generate random salt
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_SIZE];
+        random.nextBytes(salt);
+
         PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, HASH_ITERATIONS, HASH_KEY_LENGTH);
         SecretKeyFactory factory;
         try {
             factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
             byte[] hash = factory.generateSecret(spec).getEncoded();
-            return Base64.getEncoder().encodeToString(hash);
-        } catch (NoSuchAlgorithmException e) {
+
+            return Base64.getEncoder().encodeToString(salt) + ":" + Base64.getEncoder().encodeToString(hash);
+        }
+        catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             throw new RuntimeException("No such hashing algorithm");
-        } catch (InvalidKeySpecException e) {
+        }
+        catch (InvalidKeySpecException e) {
             e.printStackTrace();
             throw new RuntimeException("Invalid key specification");
         }
@@ -68,11 +70,29 @@ public abstract class AbstractUserService {
      * Verifies that an entered password matches a stored password hash.
      *
      * @param enteredPassword the entered password
-     * @param passwordHash    the stored password hash
+     * @param storedPasswordHash    the stored password hash
      * @return {@code true} if the password is correct, otherwise {@code false}
      */    
-    protected boolean isUserPasswordCorrect(String enteredPassword, String passwordHash) {
-        String enteredPasswordHash = getPasswordHash(enteredPassword);
-        return enteredPasswordHash.equals(passwordHash);
+    protected boolean isUserPasswordCorrect(String enteredPassword, String storedPasswordHash) {
+        String[] hashParts = storedPasswordHash.split(":");
+        byte[] salt = Base64.getDecoder().decode(hashParts[0]);
+        byte[] storedHash = Base64.getDecoder().decode(hashParts[1]);;
+
+        PBEKeySpec spec = new PBEKeySpec(enteredPassword.toCharArray(), salt, HASH_ITERATIONS, HASH_KEY_LENGTH);
+        SecretKeyFactory factory;
+        try {
+            factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            byte[] testHash = factory.generateSecret(spec).getEncoded();
+
+            return Arrays.equals(storedHash, testHash);
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            throw new RuntimeException("No such hashing algorithm");
+        }
+        catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Invalid key specification");
+        }
     }
 }
