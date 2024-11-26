eb6208fc77e542c461c8598e2118bb6df981b28e
Dzmitry Haron <pitbull9846@gmail.com>
2024-11-26 20:11:09 +0300

    Add working pricing servlet

--- hotel-web-app/src/main/java/by/bsuir/hotelwebapp/servlet/PricingServlet.java
+++ hotel-web-app/src/main/java/by/bsuir/hotelwebapp/servlet/PricingServlet.java
@@ -10,6 +10,7 @@ import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import by.bsuir.hotelwebapp.config.HibernateSession;
 import by.bsuir.hotelwebapp.config.ThymeleafConfig;
+import by.bsuir.hotelwebapp.dto.request.RoomTypeRequestDTO;
 import by.bsuir.hotelwebapp.dto.response.RoomTypeResponseDTO;
 import by.bsuir.hotelwebapp.dto.response.UserResponseDTO;
 import by.bsuir.hotelwebapp.service.RoomTypeService;
@@ -19,7 +20,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/pricing")
+@WebServlet({"/pricing/*"})
 public class PricingServlet extends HttpServlet {
     private JakartaServletWebApplication jakartaWebApp;
     private ITemplateEngine templateEngine;
@@ -32,35 +33,41 @@ public class PricingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String path = req.getPathInfo();
-        
-        switch (path) {
-            case null:
-                doGetRootPath(req, resp);
-                break;
-            case "/submit":
-                doGetSubmitPath(req, resp);
-                break;
-            default:
-                break;
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doPost(req, resp);
-    }
-
-    private void doGetRootPath(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=UTF-8");
-        WebContext context = new WebContext(jakartaWebApp.buildExchange(req, resp), req.getLocale());
-
         UserResponseDTO user = (UserResponseDTO) req.getSession().getAttribute("user");
         if (user == null || !user.isAdmin()) {
             resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Access is forbidden");
             return;
         }
+        
+        String path = req.getPathInfo();
+        if (path == null || path.equals("/")) {
+            doGetRootPath(req, resp);
+        }
+        else if (path.equals("/submit")) {
+            doGetSubmitPath(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserResponseDTO user = (UserResponseDTO) req.getSession().getAttribute("user");
+        if (user == null || !user.isAdmin()) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Access is forbidden");
+            return;
+        }
+
+        String path = req.getPathInfo();
+        if (path == null || path.equals("/")) {
+            return;
+        }
+        else if (path.equals("/submit")) {
+            doPostSubmitPath(req, resp);
+        }
+    }
+
+    private void doGetRootPath(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        WebContext context = new WebContext(jakartaWebApp.buildExchange(req, resp), req.getLocale());
 
         try (StatelessSession hibernateSession = HibernateSession.getStatelessSession()) {
             hibernateSession.beginTransaction();
@@ -79,7 +86,39 @@ public class PricingServlet extends HttpServlet {
     }
 
     private void doGetSubmitPath(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        WebContext context = new WebContext(jakartaWebApp.buildExchange(req, resp), req.getLocale());
 
+        Long id = Long.parseLong(req.getParameter("id"));
+        String name = req.getParameter("name");
+        Integer price = Integer.parseInt(req.getParameter("price"));
+        Integer freeRooms = Integer.parseInt(req.getParameter("freeRooms"));
+        RoomTypeRequestDTO roomType = new RoomTypeRequestDTO(id, name, price, freeRooms);
+
+        context.setVariable("roomType", roomType);
+
+        templateEngine.process("pricing/submit", context, resp.getWriter());
     }
     
+    private void doPostSubmitPath(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+
+        try (StatelessSession hibernateSession = HibernateSession.getStatelessSession()) {
+            Long id = Long.parseLong(req.getParameter("id"));
+            String name = req.getParameter("name");
+            Integer price = Integer.parseInt(req.getParameter("price"));
+            Integer freeRooms = Integer.parseInt(req.getParameter("freeRooms"));
+            RoomTypeRequestDTO roomType = new RoomTypeRequestDTO(id, name, price, freeRooms);
+            
+            hibernateSession.beginTransaction();
+            RoomTypeService roomTypeService = new RoomTypeService(hibernateSession);
+            roomTypeService.updateRoomType(roomType);
+            hibernateSession.getTransaction().commit();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        resp.sendRedirect(req.getContextPath() + "/pricing");
+    }
 }
